8c84dacfed855fc19d1c3743b672428b66fda7c4
Casey Foster <c@sey.me>
2013-01-18 13:48:53 -0800

    Add more skeleton files

---
+++ index.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  var $ = window.jQuery;
+
+  // Define the global namespace for the app.
+  var app = window.app = {
+
+    // Set up initial view, load initial data, etc...
+    init: function () {
+      // TODO
+    }
+  };
+
+  // Run the `init` function once the DOM is ready.
+  $(app.init);
+})();
