3b7741bbc36b8042f5257bd47901befe738e1282
Tim <tim@tabdeveloper.com>
2013-01-26 13:01:12 -0500

    fixing router again

--- routers/router.js
+++ routers/router.js
@@ -1,43 +1,46 @@
-(function () {
+(function(window) {
   'use strict';
 
-  var $ = window.jQuery;
-  var Backbone = window.Backbone;
   var app = window.app;
 
   app.Router = Backbone.Router.extend({
-     routes:{
-       "": "manage",
-       "issue/:id": "issue"
-     },
-
     initialize: function() {
       var welcome = new app.WelcomeModalView();
       welcome.render();
     },
 
+    routes:{
+      '': 'manage',
+      'issue/:id': 'issue'
+    },
+
+    manage: function(){
+      this.resetState();
+
+      // If there is an existing view, then remove it and clear any listeners
+      this.currentView = new app.MainView();
+      $('#app-container').append(this.currentView.render().el);
+    },
+
     issue: function(issueId){
+      //this.resetState();
+
       if (!this.currentView) this.manage();
       var issue = app.board.issues.get(issueId);
       this.modal = new app.IssueModalView({model: issue});
       this.modal.render();
     },
 
-    manage: function(){
-      this.closeModal();
-
-      // If there is an existing view, then remove it and clear any listeners
-      if (this.currentView) this.currentView.remove();
-      this.currentView = new app.MainView();
-      $('#app-container').append(this.currentView.render().el);
-    },
-
-
-    closeModal: function() {
+    resetState: function() {
       if (this.modal) {
         this.modal.remove();
+        delete this.modal;
+      }
+
+      if (this.currentView) {
+        this.currentView.remove();
+        delete this.currentView;
       }
     }
   });
-
-})();
+})(this);
