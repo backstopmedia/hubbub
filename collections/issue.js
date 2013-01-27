d6d75486dbf7a6b3ad112246ccab571686f5ac8a
Dave Tonge <dave@simplecreativity.co.uk>
2013-01-27 21:43:08 +0000

    More tests and refactored issue collection

--- collections/issue.js
+++ collections/issue.js
@@ -22,20 +22,24 @@
     setFilter: function(parent, testKey, testValue) {
       var self = this;
       this.listenTo(parent, {
+        // When a model is added to the parent, add it to this collection if it matches the test key / value
         add: function(model) {
           if (model.get(testKey) === testValue) {
             self.add(model);
           }
         },
-        remove: function(model) {
-          self.remove(model);
-        }
+        // When a model is removed from the parent, remove it from this collection. This method is a "no op"  if the
+        // collection doesn't contain the model.
+        remove: self.remove
       });
+      // Listen to change events on the models
       this.listenTo(parent, 'change:' + testKey, function(model, value) {
         if(value === testValue){
-          if (!self.get(model)) self.add(model);
+          // If the new value matches the test value then add the model
+          self.add(model);
         } else {
-          if (self.get(model)) self.remove(model);
+          // If the value doesn't match the test value then remove the model
+          self.remove(model);
         }
       });
       parent.each(function(model) {
--- test/generic.js
+++ test/generic.js
@@ -6,6 +6,8 @@ window.jQuery(function () {
   var ok = window.ok;
   var test = window.test;
   var equal = window.equal;
+  var deepEqual = window.deepEqual;
+
   /* Test Data Generator:
    JSON.stringify(app.board.issues.map(function(a){
      return {
@@ -19,10 +21,10 @@ window.jQuery(function () {
    }));
    */
   var testData = [
-    {"id": 10338616, "title": "Comparator and fat arrow", "number": 2195, "repoId": 952189, "category": "doing", "created_at": "2013-01-26T14:35:16Z"},
-    {"id": 10341232, "title": "trigger calls unbinded event handlers", "number": 2198, "repoId": 952189, "category": "todo", "created_at": "2013-01-26T18:26:53Z"},
-    {"id": 10339785, "title": "All Code refactoring", "number": 2196, "repoId": 952189, "category": "doing", "created_at": "2013-01-26T16:29:40Z"},
-    {"id": 10172489, "title": "Reverting changes from #2003 and 1f3f45252f", "number": 2173, "repoId": 952189, "category": "done", "created_at": "2013-01-21T21:36:58Z"}
+    {"id": 10338616, "title": "Comparator and fat arrow", "number": 2195, "repoId": 952189, "category": "doing", "comments": 5, "created_at": "2013-01-26T14:35:16Z"},
+    {"id": 10341232, "title": "trigger calls unbinded event handlers", "number": 2198, "repoId": 952189, "category": "todo", "comments": 21, "created_at": "2013-01-26T18:26:53Z"},
+    {"id": 10339785, "title": "All Code refactoring", "number": 2196, "repoId": 952189, "category": "doing", "comments": 1, "created_at": "2013-01-26T16:29:40Z"},
+    {"id": 10172489, "title": "Reverting changes from #2003 and 1f3f45252f", "number": 2173, "repoId": 952189, "category": "done", "comments": 3, "created_at": "2013-01-21T21:36:58Z"}
   ];
 
   module('Generic Tests', {
@@ -31,14 +33,14 @@ window.jQuery(function () {
     }
   });
 
-  test('sort by title', function () {
+  test('collection: sort by title', function () {
     var issues = new app.Issue.Collection(testData);
     equal(issues.at(0).get("title"), "trigger calls unbinded event handlers"); // This is the first item before sorting
     var sorted = issues.sortBy("title");
     equal(sorted[0].get("title"), "All Code refactoring");  // After sorting this item is first
   });
 
-  test('sort by longest title', function () {
+  test('collection: sort by longest title', function () {
     var issues = new app.Issue.Collection(testData);
     var sorted = issues.sortBy(function(issue) {
       return -1 * issue.get("title").length;
@@ -46,7 +48,7 @@ window.jQuery(function () {
     equal(sorted[0].get("title"), "Reverting changes from #2003 and 1f3f45252f");  // After sorting this item is first
   });
 
-  test('filter by title contains "event"', function () {
+  test('collection: filter by title contains "event"', function () {
     var issues = new app.Issue.Collection(testData);
     var filtered = issues.filter(function(issue) {
       return issue.get("title").indexOf("event") !== -1;
@@ -55,7 +57,7 @@ window.jQuery(function () {
     equal(filtered[0].id, 10341232);  // Confirm correct model is returned
   });
 
-  test('map & pluck', function () {
+  test('collection: map & pluck', function () {
     var issues = new app.Issue.Collection(testData);
     var mapped = issues.map(function(issue) {
       return issue.get("number");
@@ -65,5 +67,54 @@ window.jQuery(function () {
     equal(plucked.length, 4); // Should have 4 values
     equal(plucked[0], issues.at(0).get("number")); // First value should be equal to the number property of the first model
   });
+
+  test('collection: some', function () {
+    var issues = new app.Issue.Collection(testData);
+    var hasToDoIssues = issues.some(function(issue) {
+      return issue.get("category") === "todo";
+    });
+    var hasRejectedIssues = issues.some(function(issue) {
+      return issue.get("category") === "rejected";
+    });
+    equal(hasToDoIssues, true);  // This variable should be true as there are issues with a todo category
+    equal(hasRejectedIssues, false);  // This variable should be false as there are no issues with a rejected category
+  });
+
+  test('collection: reduce', function () {
+    var issues = new app.Issue.Collection(testData);
+    var reduceIterator = function(sum, issue) {
+      return sum + issue.get("comments");
+    };
+    var numberOfComments = issues.reduce(reduceIterator, 0); // We pass an iterator function and the starting "memo" value
+    equal(numberOfComments, 30);  // The total number of comments from all the issues.
+  });
+
+  test('collection: max & min', function () {
+    var issues = new app.Issue.Collection(testData);
+    var iterator = function(issue) {
+      return issue.get("comments");
+    };
+    var mostCommentsIssue = issues.max(iterator);  // We can use the same iterator for both the min and max methods
+    var leastCommentsIssue = issues.min(iterator);
+    equal(mostCommentsIssue.id, 10341232);
+    equal(leastCommentsIssue.id, 10339785);
+  });
+
+  test('collection: groupBy & countBy', function () {
+    var issues = new app.Issue.Collection(testData);
+    var iterator = function(issue) {
+      return issue.get("category");
+    };
+    var grouped = issues.groupBy(iterator);
+    var counts = issues.countBy(iterator);
+    deepEqual({
+      done:1,
+      doing:2,
+      todo:1
+    }, counts);
+    equal(counts.doing, grouped.doing.length);
+  });
+
+
 
 });
